670b3f6dc95e7a2ba854f4a6b9df456ffebb5845
XphantomX <geguna27@gmail.com>
2023-01-18 00:49:31 +0400

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+* ChatGPT Api for D  
+```Soon i'll do this thing  it wont take a long time ;```
---
+++ main.d
@@ -0,0 +1,12 @@
+import std.stdio;
+import std.string;
+
+
+
+void main(){
+	
+	
+string D = "Sooner or later i'll  use chatgpt and will create little api which will help me to use chatgpt without going at their website ;) ";
+	
+	writeln(D);
+}
